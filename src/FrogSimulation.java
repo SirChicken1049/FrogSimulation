c2cc09ee09294c21c460e2798076eade2033b58c
EvanBurman <evanb77@nycstudents.net>
2025-11-21 11:45:56 -0500

    frogsim big finish

--- src/FrogSimulation.java
+++ src/FrogSimulation.java
@@ -8,29 +8,31 @@ public class FrogSimulation {
     }
 
     private int hopDistance() {
-        int total = 0;
-        int check = 0;
-        for (int i = 0; i <= (Math.random() * maxHops); i++){
-            check =(int) (Math.random() * 32) - 8;
-            //System.out.println(check);
-            total += check;
-        }
-        System.out.println(total);
-        return total;
+        return (int) (Math.random() * 31) - 8;
     }
 
     public boolean simulate() {
-        if (hopDistance() >= goalDistance) {
-            return true;
+        int total = 0;
+        for (int i = 0; i < maxHops; i++){
+            total += hopDistance();
+            if (total < 0) {
+                System.out.println(total);
+                return false;
+            }
+            if (total >= goalDistance) {
+                System.out.println(total);
+                return true;
+            }
         }
+        System.out.println(total);
         return false;
     }
 
     public double runSimulations(int num) {
         int goalAchieved = 0;
         for (int i = 0; i <= num; i++) {
-            boolean check2 = simulate();
-            if (check2 == true) {
+            boolean check = simulate();
+            if (check) {
                 goalAchieved += 1;
             }
         }
